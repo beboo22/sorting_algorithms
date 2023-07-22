b77d32488a7f63dfe7c4c132f6fd0653f7a828b1
Ziad-Nasser <ziadnasser177@gmail.com>
2023-07-22 22:31:26 +0300

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,18 +1,37 @@
 #include "sort.h"
-void insertion_sort_list(listint_t **list){
-        listint_t *node,*temp;
-        
-        if(list == NULL || *list == NULL || (*list)->next == NULL)
-                return NULL;
-        node = *list;
-        node = node->next;
-        while (node)
-        {
-                while (node->prev)
-                {
-                        
-                }
-                
-        }
-        
+/**
+ * insertion_sort_list -  sorts a doubly linked list
+ * @list: list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node, *temp;
+
+	if (!list)
+		return;
+	for (node = *list; node; node = node->next)
+	{
+		while (node->next && (node->next->n < node->n))
+		{
+			temp = node->next;
+				node->next = temp->next;
+				temp->prev = node->prev;
+
+				if (node->prev)
+					node->prev->next = temp;
+
+				if (temp->next)
+					temp->next->prev = node;
+
+				node->prev = temp;
+				temp->next = node;
+
+				if (temp->prev)
+					node = temp->prev;
+				else
+					*list = temp;
+
+				print_list(*list);
+		}
+	}
 }
